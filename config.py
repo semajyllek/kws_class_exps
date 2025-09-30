79657f18285fa13c2383b3b35ade0946ac76fba2
semajyllek <mrkellyjam@gmail.com>
2025-09-29 19:23:58 -0700

    config uses large

--- config.py
+++ config.py
@@ -70,7 +70,7 @@ def create_quick_config() -> ExperimentConfig:
     return ExperimentConfig(
         dataset_version='v2',
         target_keywords=['yes', 'no', 'up', 'down'],
-        dataset_sizes=['full'],
+        dataset_sizes=['large'],
         imbalance_ratios=[0.1, 0.5, 1.0],
         augmentation_methods=['none', 'synthetic', 'adversarial'],
         n_trials=2,
